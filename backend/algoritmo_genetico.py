774fc76a7059c9db4c53e50b54481c60df864883
gabryelhgb <gabryelhgb@gmail.com>
2026-08-26 20:37:12 -0300

    feat: adicionando configuracoes do algoritmo

--- backend/algoritmo_genetico.py
+++ backend/algoritmo_genetico.py
@@ -1,5 +1,23 @@
+TAMANHO_POPULACAO = 40
+QUANTIDADE_FILHOS = 20
+MAXIMO_GERACOES = 10_000
+
+QUANTIDADE_BITS = 9
+PONTO_CORTE = 4
+TAXA_MUTACAO = 0.03
+
+VALOR_MINIMO = 0
+VALOR_MAXIMO = 511
+
 def main():
     print("Algoritmo genético iniciado!")
+    print("Tamanho da população:", TAMANHO_POPULACAO)
+    print("Quantidade de filhos:", QUANTIDADE_FILHOS)
+    print("Máximo de gerações:", MAXIMO_GERACOES)
+    print("Quantidade de bits:", QUANTIDADE_BITS)
+    print("Ponto de corte:", PONTO_CORTE)
+    print("Taxa de mutação:", TAXA_MUTACAO)
+    print("Domínio:", VALOR_MINIMO, "a", VALOR_MAXIMO)
 
 
 if __name__ == "__main__":
