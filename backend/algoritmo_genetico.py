d55c5560c9d2e4b61e5b42fa3ac15b642ac4029c
gabryelhgb <gabryelhgb@gmail.com>
2026-08-31 20:21:34 -0300

    feat: adicionando funcao de aptidao

--- backend/algoritmo_genetico.py
+++ backend/algoritmo_genetico.py
@@ -1,3 +1,5 @@
+import math
+
 TAMANHO_POPULACAO = 40
 QUANTIDADE_FILHOS = 20
 MAXIMO_GERACOES = 10_000
@@ -9,7 +11,11 @@ TAXA_MUTACAO = 0.03
 VALOR_MINIMO = 0
 VALOR_MAXIMO = 511
 
+def calcular_aptidao(x):
+    return x * math.sin(x / 20) + 100
+
 def main():
+    print()
     print("Algoritmo genético iniciado!")
     print("Tamanho da população:", TAMANHO_POPULACAO)
     print("Quantidade de filhos:", QUANTIDADE_FILHOS)
@@ -19,6 +25,11 @@ def main():
     print("Taxa de mutação:", TAXA_MUTACAO)
     print("Domínio:", VALOR_MINIMO, "a", VALOR_MAXIMO)
 
+    print()
+    print("Teste da função de aptidão:")
+    print("f(0) =", calcular_aptidao(0))
+    print("f(20) =", calcular_aptidao(20))
+    print("f(100) =", calcular_aptidao(100))
 
 if __name__ == "__main__":
     main()
